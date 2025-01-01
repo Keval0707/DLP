bb41e17deb4dfdbf8767e9a4c060841b989205b6
Keval Dhabalia <115069460+Keval0707@users.noreply.github.com>
2025-01-01 18:00:22 +0530

    PR1.c

--- PR1.c
+++ PR1.c
@@ -39,3 +39,51 @@ int main() {
 
     return 0;
 }
+//2nd condition a*bb*
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isValidString(const char *inputString) {
+    int length = strlen(inputString);
+
+    if (length == 0) {
+        return false; 
+    }
+
+    int i = 0;
+    while (i < length && inputString[i] == 'a') {
+        i++;
+    }
+
+    if (i == length || inputString[i] != 'b') {
+        return false;
+    }
+    while (i < length && inputString[i] == 'b') {
+        i++;
+    }
+
+    return i == length;
+}
+
+int main() {
+    char userInput[100];
+    char userChoice;
+
+    do {
+        printf("Enter a string: ");
+        scanf("%s", userInput);
+
+        if (isValidString(userInput)) {
+            printf("%s: Valid String\n", userInput);
+        } else {
+            printf("%s: Invalid String\n", userInput);
+        }
+
+        printf("Do you want to enter another string? (y/n): ");
+        scanf(" %c", &userChoice);
+    } while (userChoice == 'y' || userChoice == 'Y');
+
+    return 0;
+}
+
